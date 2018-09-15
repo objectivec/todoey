93cea1110d4a8b6adc2d79bb5c28c80bde5a5f00
Mark Nelson <Mark_Nelson@wycliffe.org>
2018-09-15 09:13:09 -0400

    Initial Commit

---
+++ todoet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  todoet
+//
+//  Created by Mark Nelson on 9/15/18.
+//  Copyright © 2018 Just the Differences. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
